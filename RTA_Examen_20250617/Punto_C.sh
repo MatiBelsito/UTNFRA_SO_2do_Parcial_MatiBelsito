b32daa87831b61d26f23503b899804178d80711e
MatiBelsito <cuerdashop.matias64hotmail.com>
2025-06-23 03:24:19 +0000

    Commit final con  ajustes: agregado PDF, correcciones en Docker y Ansible

--- RTA_Examen_20250617/Punto_C.sh
+++ RTA_Examen_20250617/Punto_C.sh
@@ -5,6 +5,16 @@ set -e
 #IP_VM=$(ip addr show enp0s8 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)
 IP_VM="192.168.56.3"
 
+
+# === Generar info.txt con los datos del sistema ===
+MODELO=$(grep "model name" /proc/cpuinfo | head -n1 | awk -F: '{print $2}')
+FRECUENCIA=$(grep "cpu MHz" /proc/cpuinfo | head -n1 | awk -F: '{print $2}')
+
+echo "Modelo:$MODELO" > /home/mb/UTNFRA_SO_2do_Parcial_MatiBelsito/docker/web/file/info.txt
+echo "Frecuencia:$FRECUENCIA MHz" >> /home/mb/UTNFRA_SO_2do_Parcial_MatiBelsito/docker/web/file/info.txt
+
+
+
 echo "Levantando el contenedor con docker compose..."
 docker compose -f /home/mb/UTNFRA_SO_2do_Parcial_MatiBelsito/docker/docker-compose.yml up -d
 
